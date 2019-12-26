1fffa6744c73ff0b225f27ccc0ffec41a4322cc9
Jan-Niklas Bogatka <misc@bogatka.org>
2019-12-27 00:16:55 +0100

    Completed german translation and set debug = 0
    Completed translation to german and set debug to false

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
 config = {}
 config.cpt   = 128    -- Cycles Per Ticks
 config.power = 10    -- power usage
-config.debug = true  -- Debug Output (very laggy)
+config.debug = false  -- Debug Output (very laggy)
